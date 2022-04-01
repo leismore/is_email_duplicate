b75a64297d1103ba057dba890a2a400563e04125
Kyle Chine <kyle.chine@leismore.co>
2022-04-01 20:21:42 +1100

    handlers done

--- src/lib/handler/index.ts
+++ src/lib/handler/index.ts
@@ -1,5 +1,5 @@
 /**
- * Create User API
+ * Is Email Duplicate API
  * 
  * @res.locals
  * {
@@ -14,10 +14,8 @@ import { post_handler1 } from './post_handler1';
 import { post_handler2 } from './post_handler2';
 import { post_handler3 } from './post_handler3';
 import { post_handler4 } from './post_handler4';
-import { post_handler5 } from './post_handler5';
 
 export {
     all_handler1,  cors_handler,
-    post_handler1, post_handler2, post_handler3,
-    post_handler4, post_handler5
+    post_handler1, post_handler2, post_handler3, post_handler4
 };
